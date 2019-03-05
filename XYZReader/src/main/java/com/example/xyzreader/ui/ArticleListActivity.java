7bd484879846889dcd8f3a59af32eecabc149e20
jidekareem <jidekrm@gmail.com>
2019-03-05 17:22:49 -0500

    Improve UI to meet project requirement.
    * Add coordinatorLayout, appBarLayout and collapsingToolbarLayout to
    ArticleListActivity.
    * Customize Theme.Bacon.
    * Add support for constraint-layout and design.
    * Update androidSupportVersion.
    * Remove redundant elements in ArticleListActivity.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -55,10 +56,10 @@ public class ArticleListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle("Eclairs");
+//Removed
+//        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
