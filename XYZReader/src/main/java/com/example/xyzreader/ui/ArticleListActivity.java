4d8fdcd05c840df94b93c3b32acfc7761c388b01
jidekareem <jidekrm@gmail.com>
2019-03-18 15:45:24 -0400

    Add elevation and code clean up
     - Add elevation to AppBarLayout
     - Remove unused method SelectionBuilder, unused declarations in DrawInsetsFrameLayout
     - Remove unused DividerItemDecoration

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -303,6 +303,7 @@ public class ArticleListActivity extends AppCompatActivity implements
             holder.thumbnailView.setImageUrl(
                     mCursor.getString(ArticleLoader.Query.THUMB_URL),
                     ImageLoaderHelper.getInstance(ArticleListActivity.this).getImageLoader());
+
             holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
 
 
